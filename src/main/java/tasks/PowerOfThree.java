5b436c7feae0512bebe70c73445d36797fa9b6b2
Denis Gutyrchik <dgutyrchik@aligntech.com>
2020-08-24 01:33:30 +0300

    PowerOfThree GCD

--- src/main/java/tasks/PowerOfThree.java
+++ src/main/java/tasks/PowerOfThree.java
@@ -1,15 +1,30 @@
 package tasks;
 
+import java.util.Objects;
+
 public class PowerOfThree {
     public static boolean powerOfThree(int a) {
         /*
         метод должен вернуть ответ - является -ли заданое число (a) степенью тройки
          */
-        return false;
+        //System.out.println(a);
+
+        if (Objects.isNull(a) || a < 0 || a == 2){
+            return false;
+        }
+
+        else {
+            int i = 0;
+            while ( a>2 ) {
+                i = i + a % 3;
+                a = a / 3;
+                }
+            return i == 0 & a == 1;
+        }
     }
 
     public static void main(String[] arg) {
-        System.out.println(powerOfThree(0));
+        System.out.println(powerOfThree(33));
     }
 }
 

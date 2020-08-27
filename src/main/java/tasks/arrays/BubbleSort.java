255429ff91e4dd0e436cfb41aeb7198ff69dba82
Denis Gutyrchik <dgutyrchik@aligntech.com>
2020-08-27 23:19:17 +0300

    Arrays tasks

--- src/main/java/tasks/arrays/BubbleSort.java
+++ src/main/java/tasks/arrays/BubbleSort.java
@@ -1,8 +1,23 @@
 package tasks.arrays;
+
 import java.util.Arrays;
 
 public class BubbleSort {
-    public static void bubbleSort(int[] array) {
+    public static void bubbleSort(int[] array){
         // Реализовать сортировку пузырьком переданного массива
+        int i = 0;
+        int k = 0;
+        for (k = 0; k < array.length - 1; k++)
+            for (i = 0; i < array.length - 1; i++) {
+                if (array[i + 1] < array[i]) {
+                    int j = array[i + 1];
+                    array[i + 1] = array[i];
+                    array[i] = j;
+                }
+            }
+
     }
 }
+
+
+
--- src/main/java/tasks/arrays/ReverseArray.java
+++ src/main/java/tasks/arrays/ReverseArray.java
@@ -1,10 +1,19 @@
 package tasks.arrays;
 
+import java.util.ArrayList;
+
 public class ReverseArray {
-    public static String[] reverseArray(String[] array) {
+    public static String[] reverseArray(String[] array){
         //метод дожен возвращать НОВЫЙ массив который является копией этого, но записанный задом наперед
         //(последний элемент становится первым итд)
-
-        return null;
+        ArrayList<String> res = new ArrayList<>();
+        int k = array.length - 1;
+        while (k >= 0) {
+            res.add(array[k]);
+            k = k - 1;
+        }
+        int size = res.size();
+        return res.toArray(new String[size]);
     }
 }
+

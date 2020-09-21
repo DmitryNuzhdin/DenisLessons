18acd03c4ea8fcf60ec52c89d933409adb46bf9d
Denis Gutyrchik <dgutyrchik@aligntech.com>
2020-09-21 20:38:36 +0300

    CustomLinkedList in progress

--- src/main/java/tasks/collections/CustomLinkedList.java
+++ src/main/java/tasks/collections/CustomLinkedList.java
@@ -5,24 +5,81 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
+
 public class CustomLinkedList<E> implements List<E> {
     // этот класс должен быть реализацией интерфейса List (только методы size, add, get, remove)
     // реализация должна быть в виде связного списка (аналог LinkedList)
     // https://ru.wikipedia.org/wiki/%D0%A1%D0%B2%D1%8F%D0%B7%D0%BD%D1%8B%D0%B9_%D1%81%D0%BF%D0%B8%D1%81%D0%BE%D0%BA
 
+    public class Node<E> {
+        E elem;
+        Node<E> next;
+
+        private Node(E elmnt, Node<E> next) {
+            this.elem = elmnt;
+            this.next = next;
+        }
+        public E getElem() {
+            return elem;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setElem(E elem) {
+            this.elem = elem;
+        }
+
+        public void setNext(Node<E> next) {
+            this.next = next;
+        }
+    }
+
+    Node<E> firstNode;
+    int size = 0;
+
+    public Node<E> getFirstNode() {
+        return firstNode;
+    }
+
+    public void setFirstNode(Node<E> firstNode) {
+        this.firstNode = firstNode;
+    }
+
+
+    public CustomLinkedList() {
+        firstNode = new Node<E>(null,null);
+    }
 
     @Override
     public int size() {
-        return 0;
+        int i = 0;
+        Node<E> iter = new Node<E>();
+        iter = firstNode;
+        while (iter.next != null){
+            firstNode.next = iter;
+            i = i + 1;
+        }
+        return i;
     }
 
     @Override
     public E get(int index) {
+        /*int i = 0;
+        Node<E> search = new Node<E>(null,null,null)
+        while (i < index) {
+            search.
+            firstNode.getNext().getElem()
+        }*/
         return null;
     }
 
     @Override
     public boolean add(E e) {
+        Node<E> new_nod = new Node<E>(e,null);
+        firstNode.next = new_nod;
+        size = size + 1;
         return false;
     }
 
